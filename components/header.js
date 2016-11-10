8c3a1c0712e706d9eee46c5cb4f7011be3c7a360
Ivan Storck <ivan@ivanstorck.com>
2016-11-10 15:04:42 -0800

    change entry

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ export default () => (
   <Row>
     <Col xs={12}>
       <a href='/' title='Return to the homepage' id='crossfire-logo'>
-        <img src='../static/logo1000.png' style={{maxWidth: '400px'}} alt='Crossfire logo' />
+        <img className='img-responsive' src='../static/logo1000.png' style={{maxWidth: '400px'}} alt='Crossfire logo' />
       </a>
     </Col>
   </Row>
