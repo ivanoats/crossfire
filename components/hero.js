89cdbcf3ca27687b8d199fe5a6bf6b95689b39f3
Ivan Storck <ivan@ivanstorck.com>
2016-11-28 14:20:30 -0800

    home page edits

--- components/hero.js
+++ components/hero.js
@@ -4,7 +4,7 @@ import {Row, Col} from 'react-bootstrap'
 export default () => (
   <Row>
     <Col xs={12}>
-      <img className='img-responsive' src='../static/crossfire-hero-1.jpg' />
+      <img className='img-responsive' src='../static/crossfire-swiftsure-3-cropped.min.jpg' />
     </Col>
   </Row>
 )
