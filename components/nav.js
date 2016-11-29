31df4c1c7445f278eb88c93e15ccaa2a09a690ee
Ivan Storck <ivan@ivanstorck.com>
2016-11-29 14:08:58 -0800

    link on logo

--- components/nav.js
+++ components/nav.js
@@ -5,14 +5,16 @@ import {Navbar, Nav, NavItem} from 'react-bootstrap'
 export default () => (
   <Navbar style={{backgroundColor: '#fff', borderColor: '#fff'}}>
     <Navbar.Header>
-      <img className='img-responsive' src='../static/logo1000.png' style={{maxWidth: '400px'}} alt='Crossfire logo' />
+      <Link href="/">
+        <img className='img-responsive' src='../static/logo1000.png' style={{maxWidth: '400px'}} alt='Crossfire logo' />
+      </Link>
     </Navbar.Header>
     <Nav pullRight={true}>
-      <NavItem bsClass='div' eventKey={2}><Link href='crew'>Crew</Link></NavItem>
+      <li role='presentation'><Link className='button' href='crew'>Crew</Link></li>
       <NavItem eventKey={3}>The Boat</NavItem>
-      <NavItem eventKey={4} href='#'>Schedule</NavItem>
-      <NavItem eventKey={5} href='#'>Training</NavItem>
-      <NavItem eventKey={6} href='#'>Contact</NavItem>
+      <NavItem eventKey={4}>Schedule</NavItem>
+      <NavItem eventKey={5}>Training</NavItem>
+      <NavItem eventKey={6}>Contact</NavItem>
     </Nav>
   </Navbar>
 )
