b746e5674ab1450c5a5b7dc1c0d01d9df9285554
Ivan Storck <ivan@ivanstorck.com>
2016-11-29 20:42:43 -0800

    boat page

--- components/nav.js
+++ components/nav.js
@@ -5,16 +5,16 @@ import {Navbar, Nav, NavItem} from 'react-bootstrap'
 export default () => (
   <Navbar style={{backgroundColor: '#fff', borderColor: '#fff'}}>
     <Navbar.Header>
-      <Link href="/">
+      <Link href='/'>
         <img className='img-responsive' src='../static/logo1000.png' style={{maxWidth: '400px'}} alt='Crossfire logo' />
       </Link>
     </Navbar.Header>
     <Nav pullRight={true}>
       <li role='presentation'><Link className='button' href='crew'>Crew</Link></li>
-      <NavItem eventKey={3}>The Boat</NavItem>
-      <NavItem eventKey={4}>Schedule</NavItem>
-      <NavItem eventKey={5}>Training</NavItem>
-      <NavItem eventKey={6}>Contact</NavItem>
+      <NavItem eventKey={3} href='boat'>The Boat</NavItem>
+      <NavItem eventKey={4} href='schedule'>Schedule</NavItem>
+      <NavItem eventKey={5} href='training'>Training</NavItem>
+      <NavItem eventKey={6} href='contact'>Contact</NavItem>
     </Nav>
   </Navbar>
 )
---
+++ pages/boat.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import {Grid, Row, Col} from 'react-bootstrap'
+import Nav from '../components/nav'
+import HeadCommon from '../components/head-common'
+// import CFFooter from '../components/footer'
+
+export default () => (
+  <Grid>
+    <HeadCommon title='The Boat' />
+    <Nav />
+    <Row>
+      <Col>
+        <h1>The Boat</h1>
+        <p>The design brief for Crossfire (ex Rima2) called for a boat that was capable of competing against the TP52's inshore buoy racing, yet strong enough to handle the most grueling offshore events.  features a high stability keel and bulb; over 50% ballast/displacement ratio. The high modulus carbon rig and composite C6 rigging adds greatly to the stability of this design.  Crossfire is a powerful yacht capable of strong upwind performance in the 9+ knot range and even more exciting downwind rides at well over 20 knots.</p>
+        <p>Crossfire was built using Pre-Preg carbon fiber with Nomex core and high-density PVC foam core in the slamming areas. The construction of this yacht was focused around extensive offshore sailing; durability was of paramount concern.</p>
+        <div className='panel panel-default'>
+          <div className='panel-heading'>Measurements:</div>
+          <div className='panel-body'>
+            <table className='table'>
+              <tr>
+                <td>LOA:</td>
+                <td>55ft 3in</td>
+              </tr>
+              <tr>
+                <td>LWL:</td>
+                <td>551ft 2in</td>
+              </tr>
+              <tr>
+                <td>Beam:</td>
+                <td>14ft 8in</td>
+              </tr>
+              <tr>
+                <td>Draft:</td>
+                <td>13 ft</td>
+              </tr>
+              <tr>
+                <td>Bridge Clearance:</td>
+                <td>90ft 5in</td>
+              </tr>
+              <tr>
+                <td>Ballast:</td>
+                <td>8050lb</td>
+              </tr>
+              <tr>
+                <td>Displacement:</td>
+                <td>20865lb</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  </Grid>
+)
