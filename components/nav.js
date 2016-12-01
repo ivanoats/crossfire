e94bc4230a5d849c886d34adb1d0452e60f377e6
Ivan Storck <ivan@ivanstorck.com>
2016-12-01 09:01:00 -0800

    navbar improvements

--- components/nav.js
+++ components/nav.js
@@ -3,18 +3,23 @@ import Link from 'next/link'
 import {Navbar, Nav, NavItem} from 'react-bootstrap'
 
 export default () => (
-  <Navbar style={{backgroundColor: '#fff', borderColor: '#fff'}}>
+  <Navbar style={{backgroundColor: '#fff', borderColor: '#fff'}} collapseOnSelect>
     <Navbar.Header>
-      <Link href='/'>
-        <img className='img-responsive' src='../static/logo1000.png' style={{maxWidth: '400px'}} alt='Crossfire logo' />
-      </Link>
+      <Navbar.Brand>
+        <Link href='/'>
+          <img src='../static/logo1000.png' style={{maxWidth: '400px'}} alt='Crossfire logo' />
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
     </Navbar.Header>
-    <Nav pullRight={true}>
-      <li role='presentation'><Link className='button' href='crew'>Crew</Link></li>
-      <NavItem eventKey={3} href='boat'>The Boat</NavItem>
-      <NavItem eventKey={4} href='schedule'>Schedule</NavItem>
-      <NavItem eventKey={5} href='training'>Training</NavItem>
-      <NavItem eventKey={6} href='contact'>Contact</NavItem>
-    </Nav>
+    <Navbar.Collapse>
+      <Nav pullRight>
+        <li role='presentation'><Link className='button' href='crew'>Crew</Link></li>
+        <NavItem eventKey={3} href='boat'>The Boat</NavItem>
+        <NavItem eventKey={4} href='schedule'>Schedule</NavItem>
+        <NavItem eventKey={5} href='training'>Training</NavItem>
+        <NavItem eventKey={6} href='contact'>Contact</NavItem>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
 )
